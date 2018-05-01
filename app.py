3300c4438dd6dc5e09810dca6196b0351d82fc78
Elise Linker <eliselinker@me.com>
2018-04-30 22:02:08 -0500

    commiting files

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, redirect
+#from flask_pymongo import PyMongo
+import pymongo
+import scrape_news
+
+app = Flask(__name__)
+
+#mongo = PyMongo(app)
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.missiontomars
+collection = db.stuff
+
+
+@app.route("/")
+def index():
+    news = db.collection.find_one()
+    return render_template("index.html", news=news)
+
+
+@app.route("/scrape")
+def scrape():
+    #listings = mongo.db.listings
+    news = db.collection
+    news_data = scrape_news.scrape()
+    news.update(
+        {},
+        news_data,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ mars_news.py
@@ -0,0 +1,21 @@
+
+# selenium 
+def scrape_news():
+    browser = webdriver.Chrome('chromedriver.exe')
+    news = {}
+    url = 'https://mars.nasa.gov/news/'
+    browser.get(url)
+    time.sleep(1)
+    
+    html = browser.page_source 
+    soup = BeautifulSoup(html, "html.parser")
+    
+    # print(soup.prettify())
+    
+    
+    news["news_title"] = soup.find("div", class_="content_title").get_text()
+    news["news_p"] = soup.find("div", class_="article_teaser_body").get_text()
+
+    return news
+
+scrape_news()
